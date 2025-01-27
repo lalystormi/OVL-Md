e4083db21fa925ada56e77e3132f0b698ea8e9f5
lalystormi <ritalaly23@gmail.com>
2025-01-27 01:31:54 +0100

    Update set.js

--- set.js
+++ set.js
@@ -1,9 +1,9 @@
 module.exports = {
-  PREFIXE: process.env.PREFIXE || "",
-  NOM_OWNER: process.env.NOM_OWNER || "Ainz",
-  NUMERO_OWNER: process.env.NUMERO_OWNER || "",
+  PREFIXE: process.env.PREFIXE || "#",
+  NOM_OWNER: process.env.NOM_OWNER || "DĒVIL Yû",
+  NUMERO_OWNER: process.env.NUMERO_OWNER || "-22941621219",
   MODE: process.env.MODE || "public",
   MENU: process.env.MENU || "https://i.ibb.co/ynx9QcZ/image.jpg",
-  SESSION_ID: process.env.SESSION_ID || "ovl",
+  SESSION_ID: process.env.SESSION_ID || "Ovl-MD_ps98Lrcy_SESSION-ID",
   DATABASE: process.env.DATABASE || 'postgresql://postgres.qnjvgxwyncnsbpfxwrbq:ovlmdmdpasse@aws-0-eu-central-1.pooler.supabase.com:6543/postgres'
 };
